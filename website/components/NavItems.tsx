cfb1f31ac32b6092c92829baf467ce192a9121b9
32byte <xlebedenko@gmail.com>
2022-03-14 12:44:00 +0100

    Added link to code

--- website/components/NavItems.tsx
+++ website/components/NavItems.tsx
@@ -1,12 +1,21 @@
-import { Group, Text } from "@mantine/core";
+import { Group, Navbar, Text } from "@mantine/core";
 import Link from "next/link";
 
 export default function NavItems() {
   return (
-    <Group direction="column">
-      <Link passHref href="/"><Text size="xl" weight={600} >Home</Text></Link>
-      <Link passHref href="/mint"><Text size="xl" weight={600} >Mint</Text></Link>
-      <Link passHref href="/promote"><Text size="xl" weight={600} >Promote</Text></Link>
-    </Group>
+    <>
+      <Navbar.Section grow>
+        <Group direction="column">
+          <Link passHref href="/"><Text size="xl" weight={600} >Home</Text></Link>
+          <Link passHref href="/mint"><Text size="xl" weight={600} >Mint</Text></Link>
+          <Link passHref href="/promote"><Text size="xl" weight={600} >Promote</Text></Link>
+        </Group>
+      </Navbar.Section>
+      <Navbar.Section>
+        <Text>
+          Code on {" "} <Text variant='link' href="https://github.com/32byte/nft-pedestal" component="a">GitHub</Text>.
+        </Text>
+      </Navbar.Section>
+    </>
   )
 }
